45f72d1bbed915a9767919410f1090629eaa1dc7
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-01 08:07:26 +0800

    Modified message connection

--- db_connection.php
+++ db_connection.php
@@ -1,16 +1,16 @@
 <?php
 $host = 'localhost';
 $db = 'hunger_relief_db';
-$user = 'root'; // Default MySQL username
-$pass = '';     // Default MySQL password for localhost (usually empty)
+$user = 'root'; 
+$pass = '';     
 
 // Create connection
 $conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
-if ($conn){
-    echo 'Connection Successful! ';
-}else{
-    die(mysqli_error($conn));
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Successfully connected to the database: " . $db;
 }
 ?>
