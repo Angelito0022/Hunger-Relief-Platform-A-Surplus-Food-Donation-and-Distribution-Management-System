201a3a96b18af574be9935770b4071460cfe616c
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-01 13:12:26 +0800

    Rename file to signin.php

--- signin.php
+++ signin.php
@@ -10,20 +10,20 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
    
-    $sql="SELECT * FROM `logintable` WHERE username='$username'";
+    $sql="SELECT * FROM `signin` WHERE username='$username'";
 
     $result = mysqli_query($conn,$sql);
     if($result){
         $num=mysqli_num_rows($result);
         if($num > 0){
-            // echo "User already exist";
+          
             $user = 1;
         }else{
-            $sql = "INSERT INTO `logintable` (username, password) VALUES ('$username', '$password')";
+            $sql = "INSERT INTO `signin` (username, password) VALUES ('$username', '$password')";
             $result = mysqli_query($conn, $sql);
 
              if ($result) {
-                // echo "Login Successful!";
+                
               $confirmationMsg = 1;
                 } else {
                     die(mysqli_error($conn));
@@ -39,7 +39,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Page</title>
+    <title>Sign In Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         body {
@@ -83,7 +83,7 @@ if ($confirmationMsg) {
     <h1 class="text-center mt-5 text-primary">" Welcome to Hunger Relief Platform "</h1>
     <div class="container">
         <div class="login-container">
-            <h2 class="login-title text-center mb-4">Log In</h2>
+            <h2 class="login-title text-center mb-4">Sign In</h2>
             <form action="login.php" method="post"> 
                 <div class="form-group mb-3">
                     <label for="username" class="form-label">Username</label>
