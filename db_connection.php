06733005f4cf2d3ffd1299aa2d9422ab8ff6a6bd
Angelito Gonzales <23-36439@g.batstate-u.edu.ph>
2024-11-01 12:55:51 +0800

    Modified db_connection

--- db_connection.php
+++ db_connection.php
@@ -8,9 +8,7 @@ $pass = '';
 $conn = new mysqli($host, $user, $pass, $db);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "Successfully connected to the database: " . $db;
-}
+if (!$conn) {
+    die(mysqli_error($conn));
+} 
 ?>
